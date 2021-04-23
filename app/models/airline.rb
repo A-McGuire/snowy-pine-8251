5385ed376d4c4f3d90caeed9f37ece7961f7cc30
Aidan McGuire <>
2021-04-23 10:21:36 -0600

    Add airlines method all_passengers

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -1,3 +1,9 @@
 class Airline < ApplicationRecord
   has_many :flights
+  has_many :passenger_flights, through: :flights
+  has_many :passengers, through: :passenger_flights
+
+  def all_passengers
+    self.passengers.where("passengers.age > ?", 17).select("passengers.*").distinct.order("passengers.age")
+  end
 end
