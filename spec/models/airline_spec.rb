41ab293d5afadb355fbc426fd490a951adb0532f
Aidan McGuire <>
2021-04-23 10:20:55 -0600

    Refactor airline spec all_passengers remove bug

--- spec/models/airline_spec.rb
+++ spec/models/airline_spec.rb
@@ -6,22 +6,23 @@ RSpec.describe Airline, type: :model do
   end
 
   describe 'instance methods' do
-    describe 'passengers' do
+    describe 'all_passengers' do
       it 'returns all passengers that have flights on airline, no duplicates, no children' do
         airline = Airline.create!(name: "Frontier")
         flight1 = airline.flights.create!(number: 1111, date: "04/01/21", departure_city: "Denver", arrival_city: "New York")
         flight2 = airline.flights.create!(number: 2222, date: "04/02/21", departure_city: "LA", arrival_city: "DC")
 
-        passenger1 = flight1.passengers.create!(name: "pass1", age:18)
-        passenger2 = flight1.passengers.create!(name: "pass2", age:50)
-        passenger3 = flight1.passengers.create!(name: "pass3", age:5)
-        
-        passenger2 = flight2.passengers.create!(name: "pass2", age:50)
-        passenger4 = flight2.passengers.create!(name: "pass4", age:17)
-        passenger5 = flight2.passengers.create!(name: "pass5", age:55)
-        passenger6 = flight2.passengers.create!(name: "pass6", age:16)
+        passenger1 = Passenger.create!(name: "pass1", age:18)
+        passenger2 = Passenger.create!(name: "pass2", age:50)
+        passenger3 = Passenger.create!(name: "pass3", age:5)
+        passenger4 = Passenger.create!(name: "pass4", age:17)
+        passenger5 = Passenger.create!(name: "pass5", age:55)
+        passenger6 = Passenger.create!(name: "pass6", age:16)
 
-        expect(airline.passengers).to eq([passenger1, passenger2, passenger5])
+        flight1.passengers.push(passenger1, passenger2, passenger3)
+        flight2.passengers.push(passenger2, passenger4, passenger5, passenger6)
+
+        expect(airline.all_passengers).to eq([passenger1, passenger2, passenger5])
       end
     end
   end
